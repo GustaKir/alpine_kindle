2cf045c4b2bda852a31c72f99cf90f38c87242fc
Gustavo Foschiera <47540491+GustaKir@users.noreply.github.com>
2025-07-05 22:58:49 -0300

    Update create_kindle_alpine_image.sh

--- create_kindle_alpine_image.sh
+++ create_kindle_alpine_image.sh
@@ -26,10 +26,12 @@ apk update
 apk upgrade
 cat /etc/alpine-release
 apk add xorg-server-xephyr xwininfo xdotool xinput dbus-x11 sudo bash nano git
-apk add desktop-file-utils gtk-engines consolekit gtk-murrine-engine caja caja-extensions marco gnome-themes-extra
+apk add desktop-file-utils gtk-engines gtk-murrine-engine caja caja-extensions marco
+apk add --update --no-cache --repository=https://dl-cdn.alpinelinux.org/alpine/v3.20/community/ consolekit
+apk add --update --no-cache --repository=https://dl-cdn.alpinelinux.org/alpine/v3.16/community/ gnome-themes-extra gnome-themes-extra-lang
 apk add \$(apk search mate -q | grep -v '\-dev' | grep -v '\-lang' | grep -v '\-doc')
 apk add \$(apk search -q ttf- | grep -v '\-doc')
-apk add onboard chromium
+apk add onboard midori
 adduser alpine -D
 echo -e \"alpine\nalpine\" | passwd alpine
 echo '%sudo ALL=(ALL) ALL' >> /etc/sudoers
@@ -43,17 +45,6 @@ git reset --hard origin/master
 dconf load /org/mate/ < ~/.config/org_mate.dconf.dump
 dconf load /org/onboard/ < ~/.config/org_onboard.dconf.dump\"
 
-echo '# Default settings for chromium. This file is sourced by /bin/sh from
-# the chromium launcher.
-
-# Options to pass to chromium.
-mouseid=\"\$(env DISPLAY=:1 xinput list --id-only \"Xephyr virtual mouse\")\"
-CHROMIUM_FLAGS='\''--force-device-scale-factor=2 --touch-devices='\''\$mouseid'\'' --pull-to-refresh=1 --disable-smooth-scrolling --enable-low-end-device-mode --disable-login-animations --disable-modal-animations --wm-window-animations-disabled --start-maximized --user-agent=Mozilla%2F5.0%20%28Linux%3B%20Android%207.0%3B%20SM-G930V%20Build%2FNRD90M%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F59.0.3071.125%20Mobile%20Safari%2F537.36'\''' > /etc/chromium/chromium.conf
-mkdir -p /usr/share/chromium/extensions
-# Install uBlock Origin
-echo '{
-	\"external_update_url\": \"https://clients2.google.com/service/update2/crx\"
-}' > /usr/share/chromium/extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm.json
 
 echo \"You're now dropped into an interactive shell in Alpine, feel free to explore and type exit to leave.\"
 sh"
